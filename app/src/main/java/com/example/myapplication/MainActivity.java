a1f97115d23319bb792871afde3eceec3369f3e4
Anna Lena <anroettl@edu.aau.at>
2021-03-07 14:46:41 +0100

    Aufgabe 2.2 jede zweite Zahl tauschen

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import static com.example.myapplication.R.id.myNumber;
+import static com.example.myapplication.R.id.serverAntwort;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private TextView text;
+
     private TextView matrikelnumber;
+    private TextView antwortServer;
+    private String matrikel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +25,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button= findViewById(R.id.button);
-        text= findViewById(R.id.textView);
+        
         matrikelnumber= findViewById(R.id.myNumber);
+        antwortServer= findViewById(R.id.serverAntwort);
 
 
        button.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
 
+             String result;
+
+                   matrikel= matrikelnumber.getText().toString();
+
+                   char[]myChar=matrikel.toCharArray();
+
+                   for(int i =0; i< myChar.length; i++) {
+                       if (i % 2 != 0) {
+                           switch (myChar[i]) {
+                               case '1':
+                                 myChar[i]='a';
+                                 break;
+                               case '2':
+                                 myChar[i]='b';
+                                 break;
+                               case '3':
+                                  myChar[i]='c';
+                                  break;
+                               case '4':
+                                   myChar[i]='d';
+                                   break;
+                               case '5':
+                                   myChar[i]='e';
+                                   break;
+                               case '6':
+                                   myChar[i]='f';
+                                   break;
+                               case '7':
+                                   myChar[i]='g';
+                                   break;
+                               case '8':
+                                   myChar[i]='h';
+                                   break;
+                               case '9':
+                                   myChar[i]='i';
+                                   break;
+                               case '0':
+                                   myChar[i]='j';
+                                   break;
+                               
+                               
+                           }
+
+                       }
+
+
+                   }
+
+                   result= new String(myChar);
+               Log.d("t", "onClick: "+result);
+                     antwortServer.setText(result);
+
            }
        });
 
+        }
+
 
     }
-}
